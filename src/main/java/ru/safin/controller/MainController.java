388acf6cccbe102615602e9fc742d7312a72a6b9
stimur1709 <stimur1709@mail.ru>
2022-06-22 17:39:38 +0700

    Добавлена конфигурация spring приложения

---
+++ src/main/java/ru/safin/controller/MainController.java
@@ -0,0 +1,13 @@
+package ru.safin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    public String getMain() {
+        return "index";
+    }
+}
